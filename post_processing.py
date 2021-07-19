d8692ae893eea4cad5ab9377991c28cb0aefd2bf
Wang Zilin <zilin_wang@mymail.sutd.edu.sg>
2021-07-19 22:37:11 +0800

    replace remove_small_objects

--- post_processing.py
+++ post_processing.py
@@ -21,6 +21,45 @@ from skimage.segmentation import watershed
 
 from util_hv_map import get_bounding_box
 
+def remove_small_objects(pred, min_size=64, connectivity=1):
+    """Remove connected components smaller than the specified size.
+    This function is taken from skimage.morphology.remove_small_objects, but the warning
+    is removed when a single label is provided. 
+    Args:
+        pred: input labelled array
+        min_size: minimum size of instance in output array
+        connectivity: The connectivity defining the neighborhood of a pixel. 
+    
+    Returns:
+        out: output array with instances removed under min_size
+    """
+    out = pred
+
+    if min_size == 0:  # shortcut for efficiency
+        return out
+
+    if out.dtype == bool:
+        selem = ndimage.generate_binary_structure(pred.ndim, connectivity)
+        ccs = np.zeros_like(pred, dtype=np.int32)
+        ndimage.label(pred, selem, output=ccs)
+    else:
+        ccs = out
+
+    try:
+        component_sizes = np.bincount(ccs.ravel())
+    except ValueError:
+        raise ValueError(
+            "Negative value labels are not supported. Try "
+            "relabeling the input with `scipy.ndimage.label` or "
+            "`skimage.morphology.label`."
+        )
+
+    too_small = component_sizes < min_size
+    too_small_mask = too_small[ccs]
+    out[too_small_mask] = 0
+
+    return out
+
 def __proc_np_hv(pred):
     """Process Nuclei Prediction with XY Coordinate Map.
     Args:
@@ -42,7 +81,7 @@ def __proc_np_hv(pred):
     blb = np.array(pred_binary, dtype=np.int32)
 
     blb = measurements.label(blb)[0]
-    blb = morph.remove_small_objects(blb, min_size=10)
+    blb = remove_small_objects(blb, min_size=10)
     blb[blb > 0] = 1  # background is 0 already
 
     h_dir = cv2.normalize(
